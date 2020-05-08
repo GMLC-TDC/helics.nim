c061ad320d0e27dbbe8b7aaebdc6532a2aaa2be2
Dheepak Krishnamurthy <me@kdheepak.com>
2020-05-07 21:01:06 -0600

    Add HELICS_INSTALL environment variable support

--- helics/helics.nim
+++ helics/helics.nim
@@ -1,1 +1,12 @@
 include private/helics
+
+const helics_install_path = getEnv("HELICS_INSTALL")
+
+static:
+  putEnv("HELICS_INSTALL", helics_install_path)
+
+when defined(linux) or defined(macosx):
+  block:
+    {.passL: """-Wl,-rpath,'""" & helics_install_path & """/lib/'""".}
+    {.passL: """-Wl,-rpath,'$ORIGIN/""" & helics_install_path & """/lib/'""".}
+    {.passL: """-Wl,-rpath,'$ORIGIN'""".}
