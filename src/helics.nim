19697316f50f6c044198bf088e98a0f9c26a48d4
Dheepak Krishnamurthy <me@kdheepak.com>
2020-05-08 12:18:16 -0600

    Use loader_path and executable_path

--- src/helics.nim
+++ src/helics.nim
@@ -5,11 +5,19 @@ const helics_install_path = getEnv("HELICS_INSTALL")
 static:
   putEnv("HELICS_INSTALL", helics_install_path)
 
-when defined(linux) or defined(macosx):
+when defined(linux):
   block:
     {.passL: """-Wl,-rpath,'""" & helics_install_path & """/lib/'""".}
     {.passL: """-Wl,-rpath,'$ORIGIN""" & helics_install_path & """/lib/'""".}
     {.passL: """-Wl,-rpath,'$ORIGIN'""".}
+
+when defined(macosx):
+  block:
+    {.passL: """-Wl,-rpath,'""" & helics_install_path & """/lib/'""".}
+    {.passL: """-Wl,-rpath,'@loader_path""" & helics_install_path & """/lib/'""".}
+    {.passL: """-Wl,-rpath,'@loader_path'""".}
+    {.passL: """-Wl,-rpath,'@executable_path""" & helics_install_path & """/lib/'""".}
+    {.passL: """-Wl,-rpath,'@executable_path'""".}
 
 when defined(windows):
   const helicsSharedLib* = "helicsSharedLib.dll"
