8f90bd08f6bb997c1416105f6e5ca56f90694597
Dheepak Krishnamurthy <me@kdheepak.com>
2020-05-18 11:45:15 -0600

    Use --noHeader flag

--- helics.nim
+++ helics.nim
@@ -95,4 +95,4 @@ elif defined(posix):
 else:
   static: doAssert false
 
-cImport(srcDir/folder/"/include/helics/chelics.h", recurse = true, dynlib = "dynlibFile", flags="-f:ast2")
+cImport(srcDir/folder/"/include/helics/chelics.h", recurse = true, dynlib = "dynlibFile", flags="-f:ast2 --noHeader")
